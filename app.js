b2174ba9ac25ebad5791e22cd0155ee7096e5b30
Samuel Damilare <48826043+DPsalmist@users.noreply.github.com>
2021-05-15 22:41:42 +0100

    Update app.js

--- app.js
+++ app.js
@@ -112,33 +112,34 @@ class Store {
         // setItem takes key and string_of_value
         localStorage.setItem('books', JSON.stringify(books));
     }
-//       // Remove Book From Store
-//     static removeBook(isbn){
-//         const books = Store.getBooks();
-//         // Loop through to see isbn
-//         books.forEach((book, index) => {
-//             // Check if retrieved book isbn = isbn
-//             if(book.isbn === isbn){
-//                 // Remove a book index
-//                 books.splice(index, 1);
-//             }
-//         });
-//         // Reset localStorage after removing a book
-//         localStorage.setItem('books', JSON.stringify(books));
-//     }
+      // Remove Book From Store
+    static removeBook(isbn){
+        const books = Store.getBooks();
+        // Loop through to see isbn
+        books.forEach((book, index) => {
+            // Check if retrieved book isbn = isbn
+            if(book.isbn === isbn){
+                // Remove a book index
+                books.splice(index, 1);
+            }
+        });
+        // Reset localStorage after removing a book
+        localStorage.setItem('books', JSON.stringify(books));
+    }
+}
 
     // Traversy
-    static removeBook(isbn) {
-        const books = Store.getBooks();
+//     static removeBook(isbn) {
+//         const books = Store.getBooks();
     
-        books.forEach((book, index) => {
-          if(book.isbn === isbn) {
-            books.splice(index, 1);
-          }
-        });
-        localStorage.setItem('books', JSON.stringify(books));
-      }
-}
+//         books.forEach((book, index) => {
+//           if(book.isbn === isbn) {
+//             books.splice(index, 1);
+//           }
+//         });
+//         localStorage.setItem('books', JSON.stringify(books));
+//       }
+
 
 // Event: Add a book
 document.querySelector('#book-form').addEventListener('submit', (e) => {
@@ -179,36 +180,3 @@ document.querySelector('#book-list').addEventListener('click', (e)=> {
     // Alert for remove book
     UI.showAlert('Book Removed Successfully!', 'success');
 });
-
-
-// Store Class: Handles Storage
-class Store {
-  static getBooks() {
-    let books;
-    if(localStorage.getItem('books') === null) {
-      books = [];
-    } else {
-      books = JSON.parse(localStorage.getItem('books'));
-    }
-
-    return books;
-  }
-
-  static addBook(book) {
-    const books = Store.getBooks();
-    books.push(book);
-    localStorage.setItem('books', JSON.stringify(books));
-  }
-
-  static removeBook(isbn) {
-    const books = Store.getBooks();
-
-    books.forEach((book, index) => {
-      if(book.isbn === isbn) {
-        books.splice(index, 1);
-      }
-    });
-
-    localStorage.setItem('books', JSON.stringify(books));
-  }
-}
